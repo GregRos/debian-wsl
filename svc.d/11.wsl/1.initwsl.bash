7ff9f6745983ac446e43def95a29140c41058f76
GregRos <gregros@gregros.dev>
2025-05-01 21:23:31 +0300

    get hostname from env variable

--- svc.d/11.wsl/1.initwsl.bash
+++ svc.d/11.wsl/1.initwsl.bash
@@ -1,4 +1,16 @@
-cp -f ./wsl.conf /etc/wsl.conf
+if [ -z "$GR_HOSTNAME" ]; then
+    GR_HOSTNAME=$(hostname)
+fi
+echo "
+    [network]
+    hostname=$GR_HOSTNAME
+    [interop]
+    appendWindowsPath = false
+    [boot]
+    command = /etc/initwsl
+    [user]
+    default = gr
+" >/etc/wsl.conf
 mkdir -p /etc/initwsl.d /var/initwsl
 cp -f ./initwsl /etc/initwsl
 chmod +x /etc/initwsl
