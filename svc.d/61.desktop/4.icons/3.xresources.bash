9eecc8f7c6074dbfc1165eef1b657c49522daed6
GregRos <gregros@gregros.dev>
2025-05-01 21:20:47 +0300

    only run xrdb if DISPLAY is set

--- svc.d/61.desktop/4.icons/3.xresources.bash
+++ svc.d/61.desktop/4.icons/3.xresources.bash
@@ -2,7 +2,9 @@ cp -rf ./config/cinnamon-cursor /usr/share/icons/cinnamon-cursor
 cp -f ./config/.Xresources "$GR_HOME/.Xresources"
 config_dest=$GR_HOME/.config/fish/conf.d/11.xresources.fish
 echo "
-    xrdb -load $GR_HOME/.Xresources
+    if set -q DISLPAY
+        xrdb -load $GR_HOME/.Xresources
+    end
 " >$config_dest
 
 fish $config_dest
